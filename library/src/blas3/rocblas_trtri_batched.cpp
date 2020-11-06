6058ba8acea1f53ed91db573ba149942a155d828
jichang <jimmy.chang@amd.com>
2020-11-06 06:58:23 +0000

    Revert "Fix for SWDEV-254253"
    This reverts commit 953b0b96b606e4bf14a0fbb1cb1c07cd78b3ef9d.

--- library/src/blas3/rocblas_trtri_batched.cpp
+++ library/src/blas3/rocblas_trtri_batched.cpp
@@ -105,7 +105,7 @@ namespace
                                                &C_tmp_host[0],
                                                batch_count * sizeof(T*),
                                                hipMemcpyHostToDevice,
-                                               handle->get_stream()));
+                                               handle->rocblas_stream));
 
             status = rocblas_trtri_large<NB, true, false, T>(handle,
                                                              uplo,
